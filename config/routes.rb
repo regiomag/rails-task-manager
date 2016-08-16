c0d61612f007fb61a8168a812d272e484dbc65dc
Michael Dusong <michael@regiomag.net>
2016-08-16 15:19:42 +0100

    wrong controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  resources :tasks
+  # # get all your tasks
+  # get '/tasks', to: "tasks#index"
+  # # get a precise task, e.g GET '/tasks/33' get task with id=3
+  # get '/tasks/:id', to: "tasks#show"
 
-  get 'pages/show'
+  # # CREATE get the form to create a new task
+  # get '/tasks/new', to: "tasks#new"
+  # # POST '/tasks': post a new task
+  # post '/tasks', to: "tasks#create"
 
-  get 'pages/new'
+  # # UPDATE GET '/tasks/:id/edit': get the form to edit an existing task
+  # get '/tasks/:id/edit', to: "tasks#edit"
+  # # PATCH '/tasks/:id': update an existing task
+  # patch '/tasks/:id', to: "tasks#update"
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # # DELETE '/tasks/:id': delete an existing task
+  # delete '/tasks/:id', to: "tasks#destroy"
+  # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-gem install faker
+Task.create(task: "Laundry", description: "Doing Laundry", manager: "Bob", jalon: "15.02.17")
+Task.create(task: "shopping", description: "Doing shopping", manager: "Alain", jalon: "15.03.17")
+Task.create(task: "Accountancy", description: "Doing Accountancy", manager: "Ania", jalon: "15.04.17")
+Task.create(task: "waking up", description: "waking up", manager: "Laeticia", jalon: "15.05.17")
 
-10.times do
-  Task.create(name: Faker::Name.name description: Faker::Name.name)
-end
+
+
+# 10.times do
+#   # Task.create([{name: Faker::Name.name}, {description: Faker::Name.name}, {manager: Faker::Name.name}, {date: Faker::Name.name}])
+# end
